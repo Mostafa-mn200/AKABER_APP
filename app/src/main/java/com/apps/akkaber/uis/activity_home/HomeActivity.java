e554f0ce624bbf518bc0385572d6a9cfe15c1e87
Mohamed <mohamedfawz2010@gmail.com>
2021-12-27 09:09:09 +0200

    mmm

--- app/src/main/java/com/apps/akkaber/uis/activity_home/HomeActivity.java
+++ app/src/main/java/com/apps/akkaber/uis/activity_home/HomeActivity.java
@@ -18,6 +18,7 @@ import androidx.navigation.ui.NavigationUI;
 import com.apps.akkaber.interfaces.Listeners;
 import com.apps.akkaber.model.UserModel;
 import com.apps.akkaber.mvvm.HomeActivityMvvm;
+import com.apps.akkaber.preferences.Preferences;
 import com.apps.akkaber.uis.activity_base.BaseActivity;
 
 import com.apps.akkaber.R;
@@ -39,7 +40,8 @@ public class HomeActivity extends BaseActivity implements Listeners.Verification
     private NavController navController;
     private HomeActivityMvvm homeActivityMvvm;
     private ActionBarDrawerToggle toggle;
-
+    private Preferences preferences;
+    private UserModel userModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +54,11 @@ public class HomeActivity extends BaseActivity implements Listeners.Verification
 
     private void initView() {
 
+        preferences = Preferences.getInstance();
+        userModel = preferences.getUserData(this);
+        if (userModel != null) {
+            binding.setModel(userModel);
+        }
         homeActivityMvvm = ViewModelProviders.of(this).get(HomeActivityMvvm.class);
 
 
@@ -79,7 +86,7 @@ public class HomeActivity extends BaseActivity implements Listeners.Verification
         homeActivityMvvm.firebase.observe(this, token -> {
             if (getUserModel() != null) {
                 UserModel userModel = getUserModel();
-              //  userModel.getData().setFirebase_token(token);
+                //  userModel.getData().setFirebase_token(token);
                 setUserModel(userModel);
             }
         });
@@ -97,32 +104,41 @@ public class HomeActivity extends BaseActivity implements Listeners.Verification
             homeActivityMvvm.updateFirebase(this, getUserModel());
         }
         binding.wallet.setOnClickListener(view -> {
-            Intent intent=new Intent(HomeActivity.this, WalletActivity.class);
-            startActivity(intent);
+            if(userModel!=null){
+            Intent intent = new Intent(HomeActivity.this, WalletActivity.class);
+            startActivity(intent);}
+            else{
+                navigationToLoginActivity();
+            }
         });
         binding.contactUs.setOnClickListener(view -> {
-            Intent intent=new Intent(HomeActivity.this, ContactUsActivity.class);
+            Intent intent = new Intent(HomeActivity.this, ContactUsActivity.class);
             startActivity(intent);
 
         });
         binding.favourite.setOnClickListener(view -> {
-            Intent intent=new Intent(HomeActivity.this, FavouriteActivity.class);
+            Intent intent = new Intent(HomeActivity.this, FavouriteActivity.class);
             startActivity(intent);
         });
         binding.llMyOrders.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(HomeActivity.this, MyOrderActivity.class);
+                Intent intent = new Intent(HomeActivity.this, MyOrderActivity.class);
                 startActivity(intent);
             }
         });
         binding.shareApp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(HomeActivity.this, ShareActivity.class);
+                Intent intent = new Intent(HomeActivity.this, ShareActivity.class);
                 startActivity(intent);
             }
         });
+    }
+
+    private void navigationToLoginActivity() {
+        Intent intent=new Intent(HomeActivity.this,LoginActivity.class);
+        startActivity(intent);
     }
 
 
